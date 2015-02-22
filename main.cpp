e848e183ec21f9bf0862f85ecb24d5039b694062
sidak <spssachdeva21695@gmail.com>
2015-02-22 23:27:51 +0530

    write pseudocode for methods in comments & make stmnt a class

--- main.cpp
+++ main.cpp
@@ -10,9 +10,17 @@ char mem[maxMem][2];
 enum typ{ udef, code, resb, resw, byte, word};
 typ memType[maxMem];
 bool memRefer[maxMem];
-struct stmnt{
-	int loc;
-	string label, opcode, opnd ;
+class stmnt{
+	public:
+		int loc;
+		string label, opcode, opnd ;
+	stmnt(int l , string lbl, string opcd, string opd){
+		loc=l;
+		label=lbl;
+		opcode=opcd;
+		opnd=opd;
+	}
+	
 };
 vector < stmnt > stmnts;
 int hex2dec(string str){
@@ -35,11 +43,76 @@ int hex2dec(char *str,int l){
     }
     return n;
 }
-void traverse(string addr){
+void traverse(string addr){ 
+	// the addr is assumed to be in hexadecimal
+	int idx = hex2dec(addr);
+	while(mem[idx][0]!='G' && mem[idx][1]!='G'){
+		if(memType[idx]==code){
+			// exit if it is already seen code
+			break;
+		}
+		else if (memType[idx]!=udef && memType[idx]!=code){
+			// belongs to data section
+			// loc -> idx
+			// opname -> based on memType
+			// value is calculate on the basis of  
+			// if word then directly use the value of the 3 bytes - hex to binary to decimal
+			
+			// make  a ctr of number of bytes in dec
+			// increment until we in memory get a sentinel value 
+			// basically until the other label is not referred 
+			// upper bound this loop with maxAddressPossible
+			// if resw then ctr/24 is value
+			// if resb then ctr is value
+			// if byte then ctr, then think if x based or c based
+		}
+		else{
+			
+			string opcd= "";
+			opcd+= mem[idx][0];
+			opcd+=mem[idx][1];
+			string opName = Optab.getInst();
+			string shrtAddr= addr.substr(2,4);
+			// add the address bits from the next instruction;
+			string opnd= "L";
+			opnd+=mem[idx+1][0];
+			opnd+=mem[idx+1][1];
+			opnd+=mem[idx+2][0];
+			opnd+=mem[idx+2][1];
+			
+			string opndAddr= opnd.substr(1,4);
+			string label = "L";
+			label+=shrtAddr;
+			// check if index based addressing 
+			//
+			// opnd+=",X";
+			// if rsub inst , clear opnd to a tab;
+			if(opName =="RSUB")opnd="\t";
+			
+			struct stmnt st(idx, label, opName, opnd);
+			stmnts.push_back(st);
+			
+			int opndInt=hex2dec(opndAddr);
+			memType[idx]=code;
+			memRefer[opndInt]=true;
+			// depending on the type of current instruction 
+			// set the mem type of opnd 
+			//memType[opndInt]=
+			
+			// if instruction is either a jump inst or jsub inst
+			// traverse(opnd.substr(1,4));
+			
+			traverse(dec2hex(idx+3));
+			
+		}
+		
+	}
 	
 }
-void printProgram(){
-	
+void writeProgram(){
+	// make a comparator based on locn 
+	// sort the vector
+	// output to file line by line 
 }
 int main(){
 	char * filename=NULL;
@@ -48,7 +121,7 @@ int main(){
 	scanf("%s",filename);
 	// use argv for it 
 	// initialise mem 
-	for(int i=0; i<66000; i++){
+	for(int i=0; i<maxMem; i++){
 		mem[i][0]='G';
 		mem[i][1]='G';
 		memType[i]= udef;
@@ -83,6 +156,6 @@ int main(){
 	}
 	firstExecAdd = tr.substr(1,6);
 	traverse(firstExecAdd);
-	printProgram();
+	writeProgram();
 	
 }
